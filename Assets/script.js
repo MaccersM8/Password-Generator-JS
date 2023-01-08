cae9f8b9cd0c77a3bfd444871d6cb8ac7c9a84f7
MaccersM8 <117213310+MaccersM8@users.noreply.github.com>
2023-01-08 20:13:53 +0000

    Created Function To Generate A Random Array Dependent On User Input

--- Assets/script.js
+++ Assets/script.js
@@ -14,7 +14,7 @@ var PasswordLength = 0;
 // Creating A Function To Collect User Input For Password Options
 function PasswordOptions() {
   // Whilst SpecialInput, NumbersInput, LowerInput And UpperInput Is False ....
-  while((SpecialInput === false) && (NumbersInput === false) && (LowerInput === false) && (UpperInput === false)) {
+  while ((SpecialInput === false) && (NumbersInput === false) && (LowerInput === false) && (UpperInput === false)) {
     // Confirm With User If Special Characters Should Be Included And Print Answer To Console For Reference
     SpecialInput = confirm("Do You Want To Include Special Characters In Your Password?");
     console.log("Special Characters = " + SpecialInput);
@@ -31,7 +31,7 @@ function PasswordOptions() {
   // Prompt User To Provide A Length For The Password And Print Answer To Console For Reference
   PasswordLength = prompt("Pick A Password Length Between 10 And 63 Characters!");
   console.log("Password Length = " + PasswordLength + " Characters");
-  
+
   // Whilst PasswordLength Is Less Than 10 Or Higher Than 63 ....
   while (PasswordLength <= 9 || PasswordLength > 63) {
     // Continue To Prompt User To Provide A Valid Length For The Password And Print Answer To Console For Reference
@@ -45,8 +45,11 @@ PasswordOptions();
 // An Empty Array For Password Specifications To Fall Into Dependent On User Input
 var ValidOptions = [];
 
+// An Empty Array For Random Password Generation To Fall Into Dependent On User Input
+var RandomOptions = [];
+
 // Function To Validate User Input And Push Relevant Arrays Into New Array
-function UserOptions () {
+function GeneratePassword() {
   // If SpecialInput === True ....
   if (SpecialInput) {
     // Push Special Array Into The ValidOptions Array
@@ -71,20 +74,67 @@ function UserOptions () {
   // If UpperInput === True ....
   if (UpperInput) {
     // Push Upper Array Into The ValidOptions Array
-    for (var UpperIndex = 0; UpperIndex < Upper.length; UpperIndex ++) {
+    for (var UpperIndex = 0; UpperIndex < Upper.length; UpperIndex++) {
       ValidOptions.push(Upper[UpperIndex]);
     }
   }
   // Print The ValidOptions Array To The Console For Reference
-  console.log("Valid Options Include: " , ValidOptions);
+  console.log("Valid Options Include: ", ValidOptions);
+
+  // Push Valid Options Into A New Array Dependent On User Input
+  for (var RandomIndex = 0; RandomIndex < PasswordLength; RandomIndex ++) {
+    RandomOptions.push(ValidOptions[Math.floor(Math.random()*ValidOptions.length)]);
+  }
+  // Print Random Options To Console For Reference
+  console.log("Random Options Include: ", RandomOptions);
+
+  // A Variable To Determine If The Random Options Includes Items From The Original Numbers Array
+  var IncludesNumbers = RandomOptions.includes(Numbers);
+  // If The User Wanted Numbers AND Random Options Doesnt Include Numbers ....
+  if (NumbersInput === true & IncludesNumbers === false) {
+    // At Index 3 Of Random Options Remove 2 Items
+    RandomOptions.splice(3, 2);
+    // At The End Of The Random Options Array Push 2 Random Items From The Numbers Array
+    RandomOptions.push(Numbers[Math.floor(Math.random()* Numbers.length)]);
+    RandomOptions.push(Numbers[Math.floor(Math.random()* Numbers.length)]);
+  }
+
+  // A Variable To Determine If The Random Options Includes Items From The Original Special Array
+  var IncludesSpecial = RandomOptions.includes(Special);
+  // If The User Wanted Special Characters AND Random Options Doesn't Include Random Characters ....
+  if (SpecialInput === true & IncludesSpecial === false) {
+    // At Index 6 Of Random Options Remove 1 Item
+    RandomOptions.splice(6 , 1);
+    // At The End Of The Random Options Array Push 1 Random Item From The Special Array
+    RandomOptions.push(Special[Math.floor(Math.random()* Special.length)]);
+  }
+
+  // A Variable To Determine If The Random Options Includes Items From The Original Lower Array
+  var IncludesLower = RandomOptions.includes(Lower);
+  // If The User Wanted Lower Characters AND Random Options Doesn't Inlcude Lower Characters ....
+  if (LowerInput === true & IncludesLower === false) {
+    // At Index 0 Of Random Options Remove 1 Item
+    RandomOptions.splice(0, 1);
+    // At The End Of The Random Options Array Push 1 Random Item From The Lower Array
+    RandomOptions.push(Lower[Math.floor(Math.random()*Lower.length)]);
+  }
+
+  // A Variable To Determine If The Random Options Includes Items From The Original Upper Array
+  var IncludesUpper = RandomOptions.includes(Upper);
+  // If The User Wanted Upper Characters AND Random Options Does'nt Include Upper Characters ....
+  if (UpperInput === true & IncludesUpper === false) {
+    // At Index 7 Of Random Options Remove 1 Item
+    RandomOptions.splice(7, 1);
+    // At The End Of The Random Options Array Push 1 Random Item From The Upper Array
+    RandomOptions.push(Upper[Math.floor(Math.random()*Upper.length)]);
+  }
+
+  // Print New RandomOptions To The Console For Reference
+  console.log("New Random Options Are: ", RandomOptions);
 }
+
 // Call UserOptions Function So It Can Run!
-UserOptions();
-
-// Function to generate password with user input
-function generatePassword() {
-
-}
+GeneratePassword();
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
